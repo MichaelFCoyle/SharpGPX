fb4916210bc73074637772edd34996225d9cf76a
Michael Coyle <michael.coyle@bluetoque.ca>
2021-02-17 00:10:10 -0800

    Added more Topografix GpxStyle extennsions

--- SharpGPX/Extensions.cs
+++ SharpGPX/Extensions.cs
@@ -6,11 +6,11 @@ namespace SharpGPX
     public static class Extensions
     {
         /// <summary>
-        /// Get a deserialized element if it exists
+        /// Get a deserialized extension if it exists
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static T Get<T>(this GPX1_1.extensionsType ext) where T : class
+        public static T Get<T>(this extensionsType ext) where T : class
         {
             if (ext.Any == null || ext.Any.Count() == 0) return null;
 
@@ -24,11 +24,60 @@ namespace SharpGPX
                 }
                 catch { }
             }
+
             return null;
         }
 
+        #region Garmin extensions
+
+        /// <summary>
+        /// Get a Garmin track extension from a GPX track
+        /// </summary>
+        /// <param name="trk"></param>
+        /// <returns></returns>
         public static GPX1_1.Garmin.TrackExtension_t GetGarminExt(this trkType trk) => trk.extensions.Get<GPX1_1.Garmin.TrackExtension_t>();
+
+        /// <summary>
+        /// Get a Garmin route extension from a GPX route
+        /// </summary>
+        /// <param name="rte"></param>
+        /// <returns></returns>
+        public static GPX1_1.Garmin.RouteExtension_t GetGarminExt(this rteType rte) => rte.extensions.Get<GPX1_1.Garmin.RouteExtension_t>();
+
+        /// <summary>
+        /// Get a Garmin waypoint extension from a GPX waypoint
+        /// </summary>
+        /// <param name="wpt"></param>
+        /// <returns></returns>
+        public static GPX1_1.Garmin.WaypointExtension_t GetGarminExt(this wptType wpt) => wpt.extensions.Get<GPX1_1.Garmin.WaypointExtension_t>();
+
+        #endregion
+
+        #region TopoGrafix extensions
+
         public static GPX1_1.Topografix.GpxStyle.lineType GetTopografixLine(this trkType trk) => trk.extensions.Get<GPX1_1.Topografix.GpxStyle.lineType>();
 
+        /// <summary>
+        /// Get a lineType from Gpx extensions
+        /// </summary>
+        /// <param name="ext"></param>
+        /// <returns></returns>
+        public static GPX1_1.Topografix.GpxStyle.lineType GetTopografixLine(this extensionsType ext) => ext.Get<GPX1_1.Topografix.GpxStyle.lineType>();
+
+        /// <summary>
+        /// Get a fillType from Gpx extensions
+        /// </summary>
+        /// <param name="ext"></param>
+        /// <returns></returns>
+        public static GPX1_1.Topografix.GpxStyle.fillType GetTopografixFill(this extensionsType ext) => ext.Get<GPX1_1.Topografix.GpxStyle.fillType>();
+
+        /// <summary>
+        /// Get a textType from Gpx extensions
+        /// </summary>
+        /// <param name="ext"></param>
+        /// <returns></returns>
+        public static GPX1_1.Topografix.GpxStyle.textType GetTopografixText(this extensionsType ext) => ext.Get<GPX1_1.Topografix.GpxStyle.textType>();
+
+        #endregion
     }
 }
